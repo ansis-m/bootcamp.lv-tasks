5f079d26fc622d3c901580d9f079578e379f2d4e
Ansis Maleckis <maleckisansis@gmail.com>
2022-03-23 13:39:30 +0200

    finished tasks 3 and 4

--- day3/task3_and_4/src/BankAccount.java
+++ day3/task3_and_4/src/BankAccount.java
@@ -13,6 +13,11 @@ public class BankAccount {
     }
 
     public void deposit(double amount){
+
+        if(amount > 5000){
+            System.out.printf("Transaction cancelled. Max deposit - 5000 | Your deposit: %.2f\n", amount);
+            return;
+        }
         if(amount >= 0d)
             balance += amount;
         else
@@ -20,17 +25,24 @@ public class BankAccount {
     }
 
     public void withdraw(double amount){
-        if(amount >= 0d)
+        if(amount >= 0d && balance >= amount)
             balance -= amount;
+        else if (amount >= 0d)
+            System.out.println("There is not enough funds.");
         else
             System.out.println("cannot withdraw negative amount");
     }
 
     public void printBalance(){
-        System.out.println("The current balance is " + balance);
+        System.out.printf("The current balance is %.2f\n", balance);
     }
 
     public void transfer(double amount, @NotNull BankAccount account){
+
+        if(this.balance < amount){
+            System.out.printf("Transfer cancelled. You are trying to transfer %.2f units, but only %.2f are available.", amount, this.balance);
+            return;
+        }
 
         this.balance -= amount;
         account.deposit(amount);
--- day3/task3_and_4/src/Main.java
+++ day3/task3_and_4/src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount(10.76);
+        BankAccount myAccount = new BankAccount(10.76); //constructor with double input
+        BankAccount otherAccount = new BankAccount(); //default constructor
         myAccount.printBalance();
+        myAccount.deposit(100.34); //deposit
+        myAccount.printBalance();
+        myAccount.withdraw(200); // withdraw more than available
+        myAccount.withdraw(20); //working withdraw
+        myAccount.printBalance();
+        myAccount.deposit(10000); //deposit more than allowed
+
+        myAccount.transfer(17.67, otherAccount); //normal transfer
+        myAccount.printBalance();
+        otherAccount.printBalance();
+
+        myAccount.transfer(200, otherAccount); //transfer more than available
+
+
     }
 }
